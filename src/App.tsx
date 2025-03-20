6fe2220e2df23dddbeb3aaab089b76c81ba065d9
Sjabloons <kristoflafaut@gmail.com>
2025-03-20 07:32:44 +0100

    5Sonner/Toast

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import ThemeProvider from "@/components/providers/themeProvider";
 import TodoList from "@/components/TodoList";
 import Layout from "@/components/Layout";
 import TodoForm from "@/components/TodoForm";
+import { Toaster } from "@/components/ui/sonner";
 
 const App = () => {
   return (
     <div>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <Layout>
+          <Toaster />
           <TodoForm />
           <TodoList />
         </Layout>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,6 +3,13 @@ import { useGetCategoriesQuery } from "@/store/categorieApi";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Badge } from "@/components/ui/badge";
 import { Category, Todo } from "@/types";
+import { toast } from "sonner";
+import { ChevronDown } from "lucide-react";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 const TodoList = () => {
   const { data: todos } = useGetTodosQuery();
   const { data: categories } = useGetCategoriesQuery();
@@ -10,14 +17,14 @@ const TodoList = () => {
     <div className="flex flex-col gap-2">
       {todos?.map((todo: Todo) => (
         <>
-          <div className="flex">
+          <div key={todo.id} className="flex">
             <Checkbox />
             <div key={todo.id}>{todo.text}</div>
 
             <div className="ml-5 flex gap-2">
               {categories?.map((category: Category) => {
                 return category.id === todo.category ? (
-                  <div>
+                  <div key={category.id}>
                     <Badge
                       style={{ backgroundColor: category.color }}
                       variant="outline"
@@ -31,6 +38,14 @@ const TodoList = () => {
               })}
             </div>
           </div>
+          <Collapsible>
+            <CollapsibleTrigger>
+              <ChevronDown />
+            </CollapsibleTrigger>
+            <CollapsibleContent>
+              <p>{todo.description}</p>
+            </CollapsibleContent>
+          </Collapsible>
         </>
       ))}
     </div>
